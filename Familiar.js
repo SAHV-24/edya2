e7fbdff104b105a80723ff7fad2b7dcac15982d3
Herrera Oficial <sergioandresherreravallejo@gmail.com>
2024-08-23 21:58:20 -0500

    deleted files

--- Familiar.js
+++
@@ -1,9 +0,0 @@
-class Familiar{
-    constructor(name, birthdate,childrens = []){
-        this.name = name
-        this.birthdate = birthdate
-        this.childrens = childrens
-    }
-}
-
-module.exports = Familiar
