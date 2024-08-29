1c91a052419a868c0b2ea561e4a9cdba6192cacd
sergio <sergioandresherreravallejo@gmail.com>
2024-08-28 19:27:45 -0500

    challenge 7

--- README.txt
+++
@@ -1,1 +0,0 @@
-Hola, este es un proyecto para Estructura de Datos y Algoritmos 2!
---
+++ vite-project/vite-project/src/FirstApp.jsx
@@ -0,0 +1,12 @@
+
+export function FirstApp(){
+
+    return(
+
+        <>
+         <h1>My first title</h1>
+         <span>10</span>
+        </>
+    )
+
+}
