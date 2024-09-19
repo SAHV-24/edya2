b3d3a412e8eb8048e20b5a783c64145dbf666e75
sergio <sergioandresherreravallejo@gmail.com>
2024-09-18 21:34:23 -0500

    final

--- challenge8/src/main.jsx
+++ challenge8/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import GiftExpertApp from './GiftExpertApp.jsx'
+import SubstractAndAdd from './components/SubstractAndAdd.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GiftExpertApp/>
-  </StrictMode>
+    <SubstractAndAdd/>
+  </StrictMode>,
 )
