39cfbfd2ec89f9316c9fef5c6c5845a182daa947
sergio <sergioandresherreravallejo@gmail.com>
2024-09-18 19:55:20 -0500

    stay

---
+++ challenge11/src/components/MultipleCustomHooks.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export function MultipleCustomHooks(URL) {
+  let [obj, setObj] = useState({
+    data: null,
+    isLoading: true,
+    hasError: false,
+  });
+
+  async function getData(URL) {
+    setTimeout(async () => {
+      try {
+        const res = await fetch(URL);
+        const { forms } = await res.json();
+
+        const newObj = { data: forms[0].name, isLoading: false, hasError: false }
+
+        setObj(newObj);
+
+      } catch (err) {
+        console.error(err);
+
+        const newObj = { data: null, isLoading: false, hasError: true }
+        setObj(newObj);
+      }
+    }, 1000);
+  }
+
+  useEffect(() => {
+    getData(URL);
+  }, [URL]);
+
+  return { ...obj };
+}
--- challenge11/src/main.jsx
+++ challenge11/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import SubstractAndAdd from './components/SubstractAndAdd.jsx'
+import Phrase from './components/Phrase.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SubstractAndAdd/>
+    <Phrase/>
   </StrictMode>,
 )
